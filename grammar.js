2c7a15b6365418fe265e2d088d275e2a56065f2e
Matthieu Dartiguenave <matthieu.dartiguenave@gmail.com>
2022-12-05 22:02:07 +0000

    Namespace

--- grammar.js
+++ grammar.js
@@ -1,8 +1,78 @@
 module.exports = grammar({
-  name: 'relaxng_compact',
+    name: 'relaxng_compact',
 
-  rules: {
-    // TODO: add the actual grammar rules
-    source_file: $ => 'hello'
-  }
+    extras: $ => [
+        /\s/
+    ],
+
+    supertypes: $ => [
+
+    ],
+    inline: $ => [
+        $._grammar_exts,
+        $._sign
+    ],
+
+    word: $ => $.identifier,
+
+    rules: {
+
+        source_file: $ => repeat($.namespace),
+        namespace: $ => seq(
+            'namespace',
+            $.identifier,
+            '=',
+            $.literal
+        ),
+        identifier: $ => /[_\p{XID_Start}][_\p{XID_Continue}]*/,
+        namespaceURILiteral: $ => choice(
+            'inherit',
+
+        ),
+
+        literal: $ => choice(
+            seq('"', '"'),
+            seq('"', $.string_content, '"')
+        ),
+
+        string_content: $ => repeat1(choice(
+            token.immediate(prec(1, /[^\\"\n]+/)),
+            $.escape_sequence
+        )),
+
+        escape_sequence: $ => token.immediate(
+            seq('\\',
+                choice(
+                    /[^xu]/,
+                    /u[0-9a-fA-F]{4}/,
+                    /u{[0-9a-fA-F]+}/,
+                    /x[0-9a-fA-F]{2}/
+                )
+            )),
+
+        keyword: $ => choice(
+            'attribute',
+            'default',
+            'datatypes',
+            'div',
+            'element',
+            'empty',
+            'external',
+            'grammar',
+            'include',
+            'inherit',
+            'list',
+            'mixed',
+            'namespace',
+            'notAllowed',
+            'parent',
+            'start',
+            'string',
+            'text',
+            'token',
+        )
+
+
+
+    }
 });
